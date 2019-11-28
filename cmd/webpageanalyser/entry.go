7eaf5658ad2637d5035296bf4e344044ca7db4dd
Fraser Tooth <frasertooth@gmail.com>
2019-11-28 17:25:23 +0900

    Added MORE CORS Handling

--- cmd/webpageanalyser/entry.go
+++ cmd/webpageanalyser/entry.go
@@ -107,15 +107,18 @@ func AnalyseWebpage(w http.ResponseWriter, r *http.Request) {
 
 	// Set CORS headers for the preflight request
 	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 
 	w.Header().Set("content-type", "application/json")
 	w.Write(output)
