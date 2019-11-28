83e73429a2f225d192fc0c147222c0c13bf90e5f
Fraser Tooth <frasertooth@gmail.com>
2019-11-28 17:19:47 +0900

    Added More CORS HAndling

--- cmd/webpageanalyser/entry.go
+++ cmd/webpageanalyser/entry.go
@@ -105,8 +105,18 @@ func AnalyseWebpage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Send
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	w.Header().Set("content-type", "application/json")
 	w.Write(output)
 }
