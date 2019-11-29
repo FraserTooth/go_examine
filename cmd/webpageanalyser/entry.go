a4c1ca6e30fff6885b3c3c32aa7a9ba8cc523a6f
Fraser Tooth <frasertooth@gmail.com>
2019-11-29 12:35:44 +0900

    Much Bigger Dictionary

--- cmd/webpageanalyser/entry.go
+++ cmd/webpageanalyser/entry.go
@@ -65,28 +65,81 @@ func grabWebpage(url string) (numberOfParagraphs int, problemWords []ProblemWord
 	numberOfParagraphs = 0
 
 	//Dictionary
+	//Remember to add a space to the start of each string
 	dictionary := []struct{
 		word string
 		message string
 	}{
 		{" think", "'Think' implies that this is an opinion, can you be sure this is a factual article?"},
+		{" belive", "'Belive' implies that this is an opinion, can you be sure this is a factual article?"},
+		
 		{" communist", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
-		{" cancer", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
 		{" immigrant", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
 		{" sex", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
+		{" millenial", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
+		{" cancer", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
+		{" vaccination", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
+		
 		{" studies", "What studies? Does this article give a source?"},
 		{" study", "What study? Does this article give a source?"},
 		{" scientist", "Which scientists? Does this article give a source?"},
+		
 		{" fact", "False articles often make appeals to 'facts', even when they are untrue. Is there a source?"},
-		{" known", "False articles often make appeals to 'facts', even when they are untrue. Is there a source?"},
+		{" truth", "False articles often make appeals to 'truth', even when they are untrue. Is there a source?"},
+		{" known", "False articles often make appeals to 'knowledge', even when they are untrue. Is there a source?"},
+		
 		{" addicted", "This is a very charged term. Is the article fair and unbaised?"},
 		{" lied", "This is a very charged term. Is the article fair and unbaised?"},
 		{" liar", "This is a very charged term. Is the article fair and unbaised?"},
-		{" millenial", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
-		{" under-25", "This term is often used in clickbait articles, is this article trying to rile you up or scare you?"},
-		{" working-class", "This is a very charged term. Is the article fair and unbaised?"},
-		{" middle-class", "This is a very charged term. Is the article fair and unbaised?"},
-		{" upper-class", "This is a very charged term. Is the article fair and unbaised?"},
+		
+		
+		{" ridiculed", "This is an objective term. Is this article trying to tell you how to feel?"},
+		{" destroyed", "This is an objective term. Is this article trying to tell you how to feel?"},
+		
+		{" psychotic", "This is a very negative term. Is the article fair and unbaised?"},
+		{" coward", "This is a very negative arged term. Is the article fair and unbaised?"},
+		
+		{" was cancelled", "Does what Twitter thinks always matter?"},
+		{" tweeted", "Twitter is a record of what people have said on twitter, not what is objectively true."},
+		{" twitter", "Twitter is a record of what people have said on twitter, not what is objectively true."},
+		
+		{" Corbyn", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Boris Johnson", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Trump", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Clinton", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Biden", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Lindsay Lohan", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Einstein", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		{" Obama", "This person is often featured in untruthful articles. Is the article representing them fairly?"},
+		
+		{" stormed", "This is very dramatic language. Is this article trying to tell you how to feel?"},
+		{" invaded", "This is very dramatic language. Is this article trying to tell you how to feel?"},
+		{" invasion", "This is very dramatic language. Is this article trying to tell you how to feel?"},
+		{" stripped", "This is very dramatic language. Is this article trying to tell you how to feel?"},
+
+		{" liberal", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" the left", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" the right", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" rightwing", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" under-25", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" working-class", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" middle-class", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" upper-class", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" leftwing", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+		{" mob", "Grouping a diverse group of people under one heading is oversimplification. Is this article trying to make you feel a certain way about this group?"},
+
+		{" total victory", "This is an objective term. Is this article trying to tell you how to feel?"},
+		{" extremely", "This is an objective term. Is this article trying to tell you how to feel?"},
+		{" very", "This is an objective term. Is this article trying to tell you how to feel?"},
+		
+		{" support the troops", "This statement is emotionally charged. Is this article emotional or factual?"},
+		{" take your guns", "This statement is emotionally charged. Is this article emotional or factual?"},
+		{" are under attack", "This statement is emotionally charged. Is this article emotional or factual?"},
+		
+		{" accused", "Accusations are not always evidence-based. Has there been an offical investigation or legal case?"},
+		
+		{" offended", "Who was offended? Articles sometimes claim people are more offended than they really are."},
+		{" triggered", "Who was offended? Articles sometimes claim people are more offended than they really are."},
 	}
 
 	//Response Prep
